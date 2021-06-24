c591cd4e3cf8a8439480febcf3ea2581767952a8
aishwaryavasu0509 <aishwaryavasu@yahoo.com>
2021-06-24 18:16:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const cors = require('cors');
+const express = require('express');
+const bodyparser = require('body-parser');
+const passport = require("passport");
+const {connect} = require("mongoose");
+const {success,error} = require("consola");
+
+// Bring in the app constants
+const {DB,PORT} = require("./config");
+
+//initialise the application
+const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(bodyparser.json());
+app.use(passport.initialize());
+
+require("./middlewares/passport")(passport);
+
+
+//User Router Middlewares
+app.use("/api/users",require("./routes/users"));
+
+const startApp = async () => {
+    try {
+        //connection with DB
+        await connect(DB, {
+            useFindAndModify:true,
+            useUnifiedTopology: true,
+            useNewUrlParser:true
+        });
+        success ({
+            message:`Sucessfully connected to the database\n ${DB}`,
+            badge:true
+        });
+
+        //start listening for the server on PORT
+
+        app.listen(PORT, () => {
+            success({message:`Server started on PORT ${PORT}`,badge: true})
+        });
+
+    } catch(err) {
+        error ({
+            message:`Unable to connect with Database\n ${err}`,
+            badge:true
+        });
+        startApp();
+    }
+    
+};
+
+startApp();
+
---
+++ routes/users.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+//Bring in the User Registration function
+const {userRegister,userLogin,checkRole,userAuth,serializeUser} = require("../utils/Auth");
+
+//Users Registration Route
+router.post("/register-user",async(req,res) => {
+    await userRegister(req.body, 'user',res);
+});
+
+//Admin Registration Route
+router.post("/register-admin",async(req,res) => {
+    await userRegister(req.body,"admin",res);
+});
+
+//Super Admin Registration Route
+router.post("/register-super-admin",async(req,res) => {
+    await userRegister(req.body,"superadmin",res);
+});
+
+//Users Login Route
+router.post("/login-user",async(req,res) => {
+    await userLogin(req.body,"user",res);
+});
+
+//Admin Login  Route
+router.post("/login-admin",async(req,res) => {
+    await userLogin(req.body,"admin",res);
+});
+
+//Super Admin Login Route
+router.post("/login-super-admin",async(req,res) => {
+    await userLogin(req.body,"superadmin",res);
+});
+
+//Profile Route
+router.get("/profile",userAuth,async(req,res) => {
+    return res.json(serializeUser(req.user));
+});
+
+//Users Protected Route
+router.get("/user-protected",userAuth,checkRole(['user']),async(req,res) => { return res.json("hello user")});
+
+//Admin Protected Route
+router.get("/admin-protected",userAuth,checkRole(['admin']),async(req,res) => {return res.json("hello admin")});
+
+//Super Admin Protected Route
+router.get("/superadmin-protected",userAuth,checkRole(['superadmin']),async(req,res) => {return res.json("super admin")});
+
+router.get("/superadminandadmin-protected",userAuth,checkRole(['superadmin','admin']),async(req,res) => {return res.json("super admin and admin")});
+
+module.exports = router;
+
+
