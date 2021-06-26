b6db0bc2e886ed6692e1aab21e299f48d3aefed5
nagarajankola <ankola.nagraj07@gmail.com>
2021-06-26 22:25:22 +0530

    added route

--- index.js
+++ index.js
@@ -23,20 +23,28 @@ require("./middlewares/passport")(passport);
 
 //User Router Middlewares
 app.post("/api/users/addToCart", async (req, res) => {
-  console.log(req.body);
-  console.log(req.body.productID);
-  const response = await Product.findOneAndUpdate(
-    {
-      _id: req.body.userID,
-    },
-    {
-      $addToSet: {
-        cartItems: req.body.productID,
+  try {
+    console.log(req.body);
+    console.log(req.body.params.productID);
+    const response = await Product.findOneAndUpdate(
+      {
+        _id: req.body.params.userID,
       },
-    }
-  );
-  // console.log(response);
-  res.send("ok");
+      {
+        $addToSet: {
+          cartItems: req.body.params.productID,
+        },
+      }
+    );
+    // console.log(response);
+    res.send("ok");
+  } catch (error) {
+    console.log(error);
+    res.status(404).send({
+      message:"fail to add"
+    })
+  }
+
 });
 app.use("/api/users", require("./routes/users"));
 app.use("/api/v1/products", productRouter);
